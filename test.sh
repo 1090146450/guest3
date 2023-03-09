592be6402a6e91329d8cb55d0bfec76fe8678c28
anlinux <anlinux@qq.com>
2023-03-09 11:30:07 +0800

    更新PID

---
+++ test.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+echo "进入虚拟环境"
+source /home/an/python_guest3/bin/activate python_guest
+echo "进入成功"
+cd /home/an/guest3
+pip3 install -r requirments.txt
+du="`netstat -ntulp|grep 8001`"
+if [[ "${du}" != "" ]];then
+ lis=$IFS
+ IFS=" "
+ ardu=($du)
+ IFS=$lis
+ for var in ${ardu[@]}
+  do
+  if [[ "${var}" =~ "/" ]]; then
+   dd=$IFS
+   IFS="/"
+   tes=($var)
+   IFS=$dd
+   echo "进程号为:"${tes}
+   kill -9 ${tes}
+   echo "kill success"
+  fi
+ done
+else
+ echo "无进程直接创建"
+fi
+uwsgi --ini uwsgi.ini
+nginx -s reload
+git add .
+git commit -m "更新PID"
+git push origin master
+
